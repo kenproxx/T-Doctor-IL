e9b32cd2f551545107aa0fda8abf2b5ee73518d4
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-07 11:31:54 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,7 @@ Route::group(['prefix' => 'profile'], function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
-Route::get('/new',[\App\Http\Controllers\fronend\HomeController::class,'index'])->name('index.new');
+
+Route::group(['prefix' => 'news'],  function () {
+    Route::get('', [\App\Http\Controllers\fronend\HomeController::class,'index'])->name('index.new');
+});
