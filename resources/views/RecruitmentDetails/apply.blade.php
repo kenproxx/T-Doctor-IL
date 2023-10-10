cc4876b2e18638c44478c8aded6b30343ddc5abd
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-10 15:00:43 +0700

    feat:apply

--- resources/views/RecruitmentDetails/apply.blade.php
+++ resources/views/RecruitmentDetails/apply.blade.php
@@ -5,9 +5,66 @@
     <div class="banner1">
     </div>
     <div class="container d-flex">
-        <div class="col-md-3">
-            <img src="{{asset('img/recruitment-img/img.png')}}" alt="">
+        <div class="apply-mr border apply-border">
+            <div class="apply-margin">
+                <div class="w-100 apply-img">
+                    <img src="{{asset('img/recruitment-img/apply_img_1.png')}}" alt="">
+                </div>
+                <div>
+                    <strong>CV Trần Đình Phi</strong>
+                    <div class="mt-1">
+                        <span>Update: </span><strong> 29/09/2023</strong>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <button class="apply-bt apply-bt_delete">Delete</button>
+                    <button class="apply-bt apply-bt_edit">Edit</button>
+                </div>
+            </div>
         </div>
+        <div class="apply-mr border apply-border">
+            <div class="apply-margin">
+                <div class="w-100 apply-img">
+                    <img src="{{asset('img/recruitment-img/apply_img_2.png')}}" alt="">
+                </div>
+                <div>
+                    <strong>CV Trần Đình Phi</strong>
+                    <div class="mt-1">
+                        <span>Update: </span><strong> 29/09/2023</strong>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <button class="apply-bt apply-bt_delete">Delete</button>
+                    <button class="apply-bt apply-bt_edit">Edit</button>
+                </div>
+            </div>
+        </div>
+        <div class="apply-mr border apply-border">
+            <div class="apply-margin">
+                <div class="w-100 apply-img">
+                    <img src="{{asset('img/recruitment-img/apply_img_3.png')}}" alt="">
+                </div>
+                <div>
+                    <strong>CV Trần Đình Phi</strong>
+                    <div class="mt-1">
+                        <span>Update: </span><strong> 29/09/2023</strong>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <button class="apply-bt apply-bt_delete">Delete</button>
+                    <button class="apply-bt apply-bt_edit">Edit</button>
+                </div>
+            </div>
+        </div>
+        <div class=" border apply-border" style="background: #D9D9D9">
+            <div class="apply-margin apply-add">
+                <strong>Add new CV</strong>
+            </div>
+        </div>
+
+
+
+
 
     </div>
 </body>
