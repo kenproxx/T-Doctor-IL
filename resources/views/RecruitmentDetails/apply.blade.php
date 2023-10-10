a45dd6dd019ee206caabd7eef897d9fbf28dd1a3
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-10 13:46:56 +0700

    feat: recruitment

---
+++ resources/views/RecruitmentDetails/apply.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+@section('title', 'Home')
+@section('content')
+<body>
+    <div class="banner1">
+    </div>
+    <div class="container d-flex">
+        <div class="col-md-3">
+            <img src="{{asset('img/recruitment-img/img.png')}}" alt="">
+        </div>
+
+    </div>
+</body>
+@endsection
