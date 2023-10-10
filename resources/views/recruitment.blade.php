ec876e8c3be0d7835b83586e592e6032f57b2de5
LongPham301 <longde345@gmail.com>
2023-10-10 10:19:31 +0700

    feat: header for user login

--- resources/views/recruitment.blade.php
+++ resources/views/recruitment.blade.php
@@ -10,6 +10,35 @@
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,400;1,700&family=Inter:wght@400;500;600;700&family=Mulish:wght@300;400;500;600;700&family=Noto+Sans+KR:wght@300;400;500;700&family=Nunito+Sans:wght@400;500&family=Poppins:wght@300&family=Roboto+Slab:wght@400;500&family=Roboto:wght@500&family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
     </head>
     <body>
+    <header class="header d-flex justify-content-around container-fluid align-items-center ">
+        <div class="nav_1">
+            <nav class="navbar">
+                <div class="d-flex flex-nowrap container-fluid">
+                    <a class="navbar-brand order-2 p-2" href="#">
+                        <img src="{{asset('img/icons_logo/image 1.png')}}" alt="Logo" width="177px" height="42px" class="d-inline-block align-text-top">
+                    </a>
+                    <a class="navbar-brand order-3 p-2" href="#"><h5><< Recruitment</h5></a>
+                </div>
+            </nav>
+        </div>
+        <div class="nav_2 d-flex justify-content-between">
+            <a href="#">My job</a>
+            <a href="#">My CV</a>
+            <a href="#">Match Up</a>
+        </div>
+        <div class=" nav_3 d-flex ">
+            <div class="btn_option">
+                <button>Business service</button>
+            </div>
+            <div class="btn_option">
+                <button>Register recruitment</button>
+            </div>
+            <div class="btn_user">
+                <button><img src="{{asset('img/user-circle.png')}}" alt="Avatar"></button>
+            </div>
+
+        </div>
+    </header>
     <div class="container-fluid">
         <div class="row">
             <div class="banner">
@@ -35,15 +64,27 @@
                 <div class="flex-fill"><button><i class="bi bi-filter"></i></button></div>
             </div>
         </div>
-        <div class="news_rank d-flex justify-content-evenly">
+        <div class="recruitment_rank d-flex flex-column">
             <div id="rank" class="d-flex">
-                <div id="title" class="d-flex">
-                    <div class="rank_title p-2 w-25">
-                        <img src="{{asset('img/platinum_rank.png')}}" width="400px" height="60px">
+                    <div class="rank_title p-2">
+                        <img src="{{asset('img/platinum_rank.png')}}" width="398px" height="60px">
                         <h2>PLATINUM</h2>
                     </div>
-                    <div class="ms-auto p-2 flex-shrink-1"><a href="#">See all</a></div>
-                </div>
+                    <div class="ms-auto p-2 "><a href="#">See all</a></div>
+            </div>
+            <div id="rank" class="d-flex">
+                    <div class="rank_title p-2">
+                        <img src="{{asset('img/premium_rank.png')}}" width="398px" height="60px">
+                        <h2>PLATINUM</h2>
+                    </div>
+                    <div class="ms-auto p-2 "><a href="#">See all</a></div>
+            </div>
+            <div id="rank" class="d-flex">
+                    <div class="rank_title p-2">
+                        <img src="{{asset('img/silver_rank.png')}}" width="398px" height="60px">
+                        <h2>PLATINUM</h2>
+                    </div>
+                    <div class="ms-auto p-2 "><a href="#">See all</a></div>
             </div>
         </div>
     </div>
