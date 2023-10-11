a67ad9810ade25232a9076d3d2d658d5a0c39b73
tranthingocanh22 <103102505+tranthingocanh22@users.noreply.github.com>
2023-10-11 17:55:23 +0700

    fix header detail

--- resources/views/layouts/partials/header_2.blade.php
+++ resources/views/layouts/partials/header_2.blade.php
@@ -1,31 +1,27 @@
 <header class="header">
-    <div class="container-fluid d-flex header-detail">
-        <div class="row">
-            <div class="col-md-4 header-detail--left d-flex justify-content-between">
+    <div class="container">
+        <div class="row header-detail">
+            <div class="col-md-4 header-detail--left d-flex justify-content-around">
                 <a class="logo" href="{{ route('home') }}">
                     <img src="{{asset('img/icons_logo/image 1.png')}}" alt="Logo" width="177px" height="42px"
                          class="d-inline-block align-text-top">
                 </a>
-                <a class="back" href="#"><h5><< Recruitment</h5></a>
+                <a class="back" href="#"><h5><i class="fa-solid fa-angles-left"></i> Recruitment</h5></a>
             </div>
-            <div class="col-md-4 header-detail--center">
-                <div class="nav_2 d-flex justify-content-between">
-                    <a href="#">My job</a>
-                    <a href="#">My CV</a>
-                    <a href="#">Match Up</a>
+            <div class="col-md-4 header-detail--center d-flex justify-content-sm-around">
+                <a class="active" href="#">My job</a>
+                <a href="{{route('recruitment.apply')}}">My CV</a>
+                <a href="#">Match Up</a>
+            </div>
+            <div class="col-md-4 header-detail--right d-flex">
+                <div class="option">
+                    <a href="#">Business service</a>
                 </div>
-            </div>
-            <div class="col-md-4 header-detail--right">
-                <div class=" nav_3 d-flex ">
-                    <div class="btn_option">
-                        <button>Business service</button>
-                    </div>
-                    <div class="btn_option">
-                        <button>Register recruitment</button>
-                    </div>
-                    <div class="btn_user">
-                        <button><img src="{{asset('img/user-circle.png')}}" alt="Avatar"></button>
-                    </div>
+                <div class="option">
+                    <a href="#">Register recruitment</a>
+                </div>
+                <div class="user">
+                    <img src="{{asset('img/user-circle.png')}}">
                 </div>
             </div>
         </div>
