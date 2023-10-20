44e81fbcf68f4ce704b3a79ba4a0037ff8638d6f
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-20 10:38:04 +0700

    style: responsive what free

--- resources/views/What-free/header-wFree.blade.php
+++ resources/views/What-free/header-wFree.blade.php
@@ -1,8 +1,11 @@
 <div class="row clinic-search">
     <div class="clinic-search--left col-md-12 d-flex justify-content-between clinic-search--center align-items-center">
-        <div class="title mobile-hidden">All <i class="bi bi-arrow-down-up"></i></div>
-        <div class="title mobile-hidden">Category <i class="bi bi-arrow-down-up"></i></div>
-        <div class="title mobile-hidden">Location <i class="bi bi-arrow-down-up"></i></div>
+        <div class="clinic-search--left col-md-6 justify-content-around mobile-hidden">
+            <div class="title mobile-hidden">All <i class="bi bi-arrow-down-up"></i></div>
+            <div class="title mobile-hidden">Category <i class="bi bi-arrow-down-up"></i></div>
+            <div class="title mobile-hidden">Location <i class="bi bi-arrow-down-up"></i></div>
+        </div>
+
         <form class="search-box col-md-5">
             <input type="search" name="focus" placeholder="Search" id="search-input" value="">
             <i class="fa-solid fa-magnifying-glass"></i>
