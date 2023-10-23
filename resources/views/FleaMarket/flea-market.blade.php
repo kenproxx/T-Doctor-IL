da22d0a3ba01246c171222f2a2e14e1482e77048
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-23 14:26:59 +0700

    style: responsive sell-product flea market

--- resources/views/FleaMarket/flea-market.blade.php
+++ resources/views/FleaMarket/flea-market.blade.php
@@ -1,13 +1,31 @@
 @extends('layouts.master')
 @section('title', 'Flea Market')
 @section('content')
-    @include('layouts.partials.header')
+    @include('layouts.partials.headerFleaMarket')
     <body>
     @include('component.banner')
 
     <div class="container mt-70">
         <div class="container pc-hidden">
-            @include('What-free.header-wFree')
+            <div class="row clinic-search">
+                <div class="clinic-search--left col-md-12 d-flex justify-content-between clinic-search--center align-items-center">
+                    <div class="clinic-search--left col-md-6 justify-content-around mobile-hidden">
+                        <div class="title mobile-hidden">All <i class="bi bi-arrow-down-up"></i></div>
+                        <div class="title mobile-hidden">Category <i class="bi bi-arrow-down-up"></i></div>
+                        <div class="title mobile-hidden">Location <i class="bi bi-arrow-down-up"></i></div>
+                    </div>
+
+                    <form class="search-box col-md-5">
+                        <input type="search" name="focus" placeholder="Search" id="search-input" value="">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </form>
+                    <div class="flex-fill"><button class="navbar-toggler border-none css-button" type="button" data-bs-toggle="offcanvas"
+                                                   data-bs-target="#filterNavbar" aria-controls="filterNavbar">
+                            <i class="bi bi-filter"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="d-flex mt-70 mobile-hidden">
             <div class="col-md-3 flea-content ">Flea market</div>
@@ -117,6 +135,79 @@
                             @include('FleaMarket.tab-product-flea')
                         </div>
                     @endfor
+                </div>
+            </div>
+        </div>
+        {{-- modal filter --}}
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="filterNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <a href="{{route('home')}}" class="offcanvas-title" id="offcanvasNavbarLabel"><img class="w-100"
+                                                                                                   src="{{asset('img/icons_logo/logo-new.png')}}"></a>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="border-radius ">
+                    <div class="flea-text">Filter</div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">All (96)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Equipments (71)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Furniture (55)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Medicine (54)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Cosmetics (49)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Furniture (53)</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label class="flea-text-gray">Others (47)</label>
+                    </div>
+                    <div class="flea-text-sp">See all categories</div>
+                </div>
+                <div class="border-radius mt-3 ">
+                    <div class="d-flex">
+                        <div class="wrapper">
+                            <header>
+                                <h2>Price</h2>
+                            </header>
+                            <div class="price-input">
+                                <div class="field">
+                                    <input type="number" class="input-min" value="0">
+                                </div>
+                                <div class="separator">-</div>
+                                <div class="field">
+                                    <input type="number" class="input-max" value="0">
+                                </div>
+                            </div>
+                            <div class="slider">
+                                <div class="progress"></div>
+                            </div>
+                            <div class="range-input">
+                                <input type="range" class="range-min" min="0" max="10000" value="2500" step="100">
+                                <input type="range" class="range-max" min="0" max="10000" value="7500" step="100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="add-cv-bt w-100 apply-bt_delete col-6">Refresh</button>
+                    <form action="#" class="col-6 pr-0">
+                        <button type="submit" class="add-cv-bt apply-bt_edit w-100">Apply</button>
+                    </form>
                 </div>
             </div>
         </div>
