db0772c51f943c8e55de48a05e539413590525f8
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-10-27 11:57:04 +0700

    fix: show clinics

--- resources/views/component/clinic.blade.php
+++ resources/views/component/clinic.blade.php
@@ -3,7 +3,6 @@
 </div>
 
 
-
 <script>
     $(document).ready(function () {
         var token = `{{ $_COOKIE['accessToken'] }}`;
@@ -42,34 +41,36 @@
                     img = img + `<img class="mr-2 w-auto h-100" src="${arrayGallery[j]}" alt="">`;
                 }
 
-                html = html + `<div class="col-md-4 mb-md-3">
-    <div class="clinic-item">
-        <a href="${urlDetail}">
-          ${item.name}
-        </a>
-        <div class="time d-flex">
-            <p>${item.open_date} - ${item.close_date} </p>
-        </div>
-        <div class="location">
-            <i class="fa-solid fa-location-dot"></i> ${item.address} - <span>3 Km</span>
-        </div>
-        <div class="service">
-            Service: Implant, Brightening, Crown(null)
-        </div>
-        <div class="star d-flex">
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-half"></i>
-            <i class="bi bi-star"></i>
-        </div>
-        <div class="img-detail row">
-        <div class="col-3 img-item d-flex">
-                ${img}
-        </div>
-                </div>
-            </div>
-        </div>`;
+                html = html + `
+                <div class="col-md-4 mb-md-3">
+                    <div class="clinic-item">
+                        <a href="${urlDetail}">
+                          ${item.name}
+                        </a>
+                        <div class="time d-flex">
+                            <p>${item.open_date} - ${item.close_date} </p>
+                        </div>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i> ${item.address} - <span>3 Km</span>
+                        </div>
+                        <div class="service">
+                            Service: Implant, Brightening, Crown(null)
+                        </div>
+                        <div class="star d-flex">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-half"></i>
+                            <i class="bi bi-star"></i>
+                        </div>
+                        <div class="img-detail row">
+                        <div class="col-3 img-item d-flex">
+                                ${img}
+                        </div>
+                                </div>
+                            </div>
+                        </div>
+                `;
             }
 
             await $('#listClinic').empty().append(html);
