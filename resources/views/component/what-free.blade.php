a23a2081d299a169ea980352ce849efba71caf4d
kienh <kienhue98@gmail.com>
2023-11-01 16:56:35 +0700

    fix: màn hình what free detail coupon

--- resources/views/component/what-free.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="border-16px color-Grey-Dark">
-    <div class="w-100"><img class="w-100" src="{{asset('img/News-event/nieng-rang.png')}}"></div>
-    <a href="{{route('what.free.detail')}}">
-        <div class="mt-3 flea-content-product">Nha khoa Blossom - Tặng 700.000đ tiền mặt và voucher 3.000.000đ khi....</div>
-        <div class="text-gray mt-2">Nha khoa Blossom - Tặng 700.000đ tiền mặt và voucher 3.000.000đ khi quay video check-in trải nghiệm trên Tiktok và Re-up Reels Facebook</div>
-    </a>
-    <div class="justify-content-between d-flex mt-2"><i class="fa-solid fa-user-group d-flex align-items-center"> <p class="flea-content-product ml-4">10/20</p></i><i class="fa-regular fa-clock d-flex align-items-center"> <p class="header-center ml-2">9/18/2023</p></i></div>
-</div>
