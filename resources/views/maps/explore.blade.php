35156ab4c999cc60069ee24287b63888d127a726
Quy Nguyen <quy.nguyen@192.168.2.10>
2023-11-02 22:28:41 +0700

    custom icons with markers

--- resources/views/maps/explore.blade.php
+++ resources/views/maps/explore.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Add a default marker to a web map</title>
+    <title>Add custom icons with Markers</title>
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js"></script>
@@ -12,23 +12,88 @@
     </style>
 </head>
 <body>
+<style>
+    .marker {
+        display: block;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        padding: 0;
+    }
+</style>
+
 <div id="map"></div>
 
 <script>
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsbmd1eWVuMTk5MyIsImEiOiJjbG9oM29qZmMxOGd4MmpuMG41OHhzb2U0In0.6zVNE3WJd-sOkWlE-fI9ow';
+    const geojson = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {
+                    'message': 'Foo',
+                    'iconSize': [60, 60]
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-66.324462, -16.024695]
+                }
+            },
+            {
+                'type': 'Feature',
+                'properties': {
+                    'message': 'Bar',
+                    'iconSize': [50, 50]
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-61.21582, -15.971891]
+                }
+            },
+            {
+                'type': 'Feature',
+                'properties': {
+                    'message': 'Baz',
+                    'iconSize': [40, 40]
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-63.292236, -18.281518]
+                }
+            }
+        ]
+    };
+
     const map = new mapboxgl.Map({
-        container: 'map',
+        container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [12.550343, 55.665957],
-        zoom: 8
+        style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+        center: [-2.81361, 36.77271], // starting position [lng, lat]
+        zoom: 13 // starting zoom
     });
 
-    // Create a default Marker and add it to the map.
-    const marker1 = new mapboxgl.Marker()
-        .setLngLat([20.9732919, 105.7542978])
-        .addTo(map);
+    // Add markers to the map.
+    for (const marker of geojson.features) {
+        // Create a DOM element for each marker.
+        const el = document.createElement('div');
+        const width = marker.properties.iconSize[0];
+        const height = marker.properties.iconSize[1];
+        el.className = 'marker';
+        el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+        el.style.width = `${width}px`;
+        el.style.height = `${height}px`;
+        el.style.backgroundSize = '100%';
 
+        el.addEventListener('click', () => {
+            window.alert(marker.properties.message);
+        });
+
+        // Add markers to the map.
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    }
 </script>
 
 </body>
