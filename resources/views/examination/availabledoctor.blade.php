77c7616dcb36d3df48a70cc4724d9d7d84f87c44
TuanAnh1386 <tuananh1386@gmail.com>
2023-11-14 22:57:49 +0700

    fix layout desktop

--- resources/views/examination/availabledoctor.blade.php
+++ resources/views/examination/availabledoctor.blade.php
@@ -11,9 +11,9 @@
                 <div class="filter_option"><p>Location <i class="bi bi-chevron-expand"></i></p></div>
                 <div class="filter_option"><p>Experience <i class="bi bi-chevron-expand"></i></p></div>
             </div>
-            <div class="filter_search flex-fill">
-                <label for="filter_search"><i class="bi bi-search"></i></label>
-                <input type="text" name="filter_search" id="filter_search" placeholder="Search for anything.....">
+            <div class="filter_search flex-fill d-flex align-content-center p-0" style="background-color: #F3F3F3!important;">
+                <button style="background-color: #F3F3F3;color: #000"><i class="bi bi-search" style="font-size: 25px; font-weight: 600"></i></button>
+                <input  style="border: " type="text" name="filter_search" id="filter_search" placeholder="Search for anything.....">
             </div>
         </div>
     </div>
@@ -24,10 +24,13 @@
             </div>
         </div>
     </div>
-    <div id="list-doctor" class="d-flex justify-content-center container">
-        <div class="card" >
+    <div id="list-doctor" class="d-flex container">
+
+            <div class="card" >
+                <div>
                 <i class="bi bi-heart"></i>
                 <img src="{{asset('img/doctor.png')}}" class="card-img-top" alt="...">
+
                 <div class="card-body">
                     <a href="{{route('examination.doctor_info')}}"><h5 class="card-title">BS Đô Văn Định</h5></a>
                     <p class="card-text">respiratory doctor</p>
@@ -35,6 +38,7 @@
                     <p class="card-text_1">Working time: <b>8:00 - 16:00</b></p>
                 </div>
             </div>
+        </div>
         <div class="card" >
                 <i class="bi bi-heart"></i>
                 <img src="{{asset('img/doctor.png')}}" class="card-img-top" alt="...">
