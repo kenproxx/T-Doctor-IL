d0595bfa9738ae08db04fab6a76a71a07ac1ffc7
TuanAnh1386 <tuananh1386@gmail.com>
2023-11-15 19:56:21 +0700

    fix:css

--- resources/views/examination/recommended.blade.php
+++ resources/views/examination/recommended.blade.php
@@ -1,24 +1,32 @@
 @extends('layouts.master')
 @section('title', 'Home')
 @section('content')
+
+    <style>
+        #list-doctor .card i {
+            left: 270px;
+        }
+    </style>
+
+
     @include('layouts.partials.header_3')
     @include('component.banner')
     <div class="d-flex justify-content-center">
         <div id="filter" class="box--1 d-flex ">
-            <div class="d-flex flex-fill">
+            <div class="d-flex flex-fill" style="gap:116px">
                 <div class="filter_option"><p>Category <i class="bi bi-chevron-expand"></i></p></div>
                 <div class="filter_option"><p>Location <i class="bi bi-chevron-expand"></i></p></div>
             </div>
-            <div class="filter_search flex-fill">
-                <label for="filter_search"><i class="bi bi-search"></i></label>
-                <input type="text" name="filter_search" id="filter_search" placeholder="Search for anything.....">
+            <div class="form-group has-search">
+                <span class="fa fa-search form-control-feedback"></span>
+                <input type="text" class="form-control" placeholder="Search for anything…">
             </div>
         </div>
     </div>
-    <div id="title" class="d-flex justify-content-center">
+    <div id="title" class="d-flex" style="padding-left: 70px">
         <div id="list-title" class="d-flex">
             <div class="list--doctor p-0">
-                <a class="back" href="{{route('examination.findmymedicine')}}"><p><i class="bi bi-arrow-left"></i>Recommended</p></a>
+                <a class="back" href="{{route('examination.findmymedicine')}}"><p><i class="bi bi-arrow-left"></i><b>Recommended</b></p></a>
             </div>
         </div>
     </div>
