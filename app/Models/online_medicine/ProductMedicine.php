3ba46271242d39bc2f60acbf8168bcaecfb9553e
kienh <kienhue98@gmail.com>
2023-11-15 10:41:24 +0700

    feat: migrate, model, enum for online medicine

---
+++ app/Models/online_medicine/ProductMedicine.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\online_medicine;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductMedicine extends Model
+{
+    use HasFactory;
+}
