f55e1ad81f186eff7b0c6c35472333d5c72a85b1
kienh <kienhue98@gmail.com>
2023-11-15 11:26:18 +0700

    feat: migrate, model, enum for new event

---
+++ app/Http/Controllers/NewEventController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewEvent;
+use Illuminate\Http\Request;
+
+class NewEventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NewEvent $newEvent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NewEvent $newEvent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, NewEvent $newEvent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NewEvent $newEvent)
+    {
+        //
+    }
+}
