8618c5d0961b3ef6bc7ad45a7af4f35965fa9204
dainqd <ngodaix5tp@gmail.com>
2023-11-16 17:23:39 +0700

    feat:checkout front-end + show checkout.

--- resources/views/component/modal-cart.blade.php
+++ resources/views/component/modal-cart.blade.php
@@ -79,7 +79,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn cancel" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn pay">Pay</button>
+                <a href="{{ route('user.checkout.index') }}" class="btn pay">Pay</a>
             </div>
         </div>
     </div>
