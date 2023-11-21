b8d8b4bd179f5e950bde985d876abc80218a703f
dainqd <ngodaix5tp@gmail.com>
2023-11-21 21:07:59 +0700

    feat: findmydoctoc.

--- app/Http/Controllers/restapi/DoctorInfoApi.php
+++ app/Http/Controllers/restapi/DoctorInfoApi.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\restapi;
 
 use App\Enums\DoctorInfoStatus;
+use App\Enums\UserStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Chat;
 use App\Models\DoctorInfo;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DoctorInfoApi extends Controller
@@ -52,5 +55,31 @@ class DoctorInfoApi extends Controller
             return response('Not found', 404);
         }
         return response()->json($doctor_infos);
+    }
+
+    public function getMyDoctor($id)
+    {
+        $message_doctor = Chat::where('from_user_id', $id)->get();
+        $array_to_ids = null;
+        foreach ($message_doctor as $message) {
+            $array_to_ids[] = $message->to_user_id;
+        }
+        $array_to_ids = array_unique($array_to_ids);
+//        dd($array_to_ids);
+        $users = User::whereIn('id', $array_to_ids)
+            ->where('status', UserStatus::ACTIVE)
+            ->get();
+        $array_doctor_ids = null;
+        foreach ($users as $user) {
+            $array_doctor_ids[] = $user->id;
+        }
+
+        $array_doctor_ids = array_unique($array_doctor_ids);
+        $doctorInfos = DoctorInfo::whereIn('created_by', $array_doctor_ids)
+            ->where('status', DoctorInfoStatus::ACTIVE)
+            ->where('created_by', '!=', $id)
+            ->get();
+
+        return response()->json($doctorInfos);
     }
 }
