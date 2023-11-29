a7f2913adfdda8f1338fbdb9932335cc9a28a96f
dainqd <ngodaix5tp@gmail.com>
2023-11-30 00:06:44 +0700

    feat: restfull api topic video.

--- app/Http/Controllers/TopicVideoController.php
+++ app/Http/Controllers/TopicVideoController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\TopicVideo;
 
 class TopicVideoController extends Controller
 {
-    //
+    public function getList()
+    {
+        return view('admin.topic-video.list');
+    }
+
+    public function detail($id)
+    {
+        $topicVideo = TopicVideo::find($id);
+        return view('admin.topic-video.detail', compact('topicVideo'));
+    }
+
+    public function create()
+    {
+        return view('admin.topic-video.create');
+    }
 }
