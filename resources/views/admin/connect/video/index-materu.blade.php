8ffee4ea9d8cab6258b4a4c5b2afcd2924305cd0
kienh <kienhue98@gmail.com>
2023-11-30 21:10:26 +0700

    fix: chat video

--- resources/views/admin/connect/video/index-materu.blade.php
+++ resources/views/admin/connect/video/index-materu.blade.php
@@ -20,7 +20,10 @@
         }
     </style>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+
+    <script src="{{ asset('build/assets/app.ec67c3ec.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('build/assets/app.2ac464e5.css') }}">
 
 </head>
 <body class="antialiased">
@@ -110,5 +113,6 @@
 </div>
 </body>
 </html>
+
 
 @endsection
