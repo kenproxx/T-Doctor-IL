f11ff5c130c966a71706f00b5348737a48ccc5da
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-12-02 01:03:58 +0700

    fix: list clinics

--- resources/views/admin/clinic/tab-list-clinics.blade.php
+++ resources/views/admin/clinic/tab-list-clinics.blade.php
@@ -5,6 +5,7 @@
         height: 80px;
     }
 </style>
+
 <div class="">
     <table class="table table-striped">
         <thead>
@@ -15,6 +16,7 @@
             <th scope="col">Address</th>
             <th scope="col">open_date</th>
             <th scope="col">close_date</th>
+            <th scope="col">status</th>
             <th scope="col">Edit</th>
         </tr>
         </thead>
@@ -23,49 +25,82 @@
     </table>
 </div>
 <script>
-    var token = `{{ $_COOKIE['accessToken'] }}`;
-    $(document).ready(function () {
-        callListProduct(token);
-        async function callListProduct(token) {
-            let accessToken = `Bearer ` + token;
-            await $.ajax({
-                url: `{{route('api.backend.clinics.list')}}`,
-                method: 'GET',
-                headers: {
-                    "Authorization": accessToken
-                },
-                success: function (response) {
-                    renderClinics(response);
-                },
-                error: function (exception) {
-                    console.log(exception)
-                }
-            });
-        }
-    });
-    async function renderClinics(res, id) {
-        let html = ``;
+    const token = `{{ $_COOKIE['accessToken'] }}`;
+    $(document).ready(() => {
 
-        for (let i = 0; i < res.length; i++) {
-            let urlEdit = `{{route('clinics.edit', ['id' => ':id'])}}`;
-            urlEdit = urlEdit.replace(':id', res[i].id);
-            let item = res[i];
+        callListProduct(token, 'CLINICS');
 
-            let gallery = item.gallery;
-            let arrayGallery = gallery.split(',')
-            let img = ``;
-            for (let j = 0; j < arrayGallery.length; j++) {
-                img = img + `<img class="mr-2 w-auto h-100" src="${arrayGallery[j]}" alt="">`;
+        $('#type_medical').on('change', function () {
+            const type = $(this).val();
+            renderClinics(token, type);
+        });
+
+        async function callListProduct(token, type) {
+            const accessToken = `Bearer ${token}`;
+            let url;
+
+            switch (type) {
+                case "PHARMACIES":
+                    url = `{{ route('api.backend.pharmacies.list') }}`;
+                    console.log(url)
+                    break;
+                case "HOSPITALS":
+                    url = `{{ route('api.backend.hospitals.list') }}`;
+                    console.log(url)
+                    break;
+                default:
+                    url = `{{ route('api.backend.clinics.list') }}`;
+                    console.log(url)
+                    break;
             }
 
-            let rowNumber = i + 1;
-            html = html + `<tr>
+            $('#listTextMedical').text(`List ${type}`);
+
+            try {
+                const response = await $.ajax({
+                    url: url,
+                    method: 'GET',
+                    headers: {
+                        Authorization: accessToken,
+                    },
+                });
+                await renderClinics(response);
+            } catch (exception) {
+                console.log(exception);
+            }
+        }
+
+        async function renderClinics(res) {
+            console.log(res)
+            let html = ``;
+
+            for (let i = 0; i < res.length; i++) {
+                let urlEdit = `{{route('clinics.edit', ['id' => ':id'])}}`;
+                urlEdit = urlEdit.replace(':id', res[i].id);
+                let item = res[i];
+                let rowNumber = i + 1;
+
+                let gallery = item.gallery;
+                let arrayGallery = [];
+
+                if (gallery) {
+                    arrayGallery = gallery.split(',');
+                }
+
+                let img = ``;
+                for (let j = 0; j < arrayGallery.length; j++) {
+                    img = img + `<img class="mr-2 w-auto h-100" src="${arrayGallery[j]}" alt="">`;
+                }
+
+
+                html = html + `<tr>
             <th scope="row">${rowNumber}</th>
             <td>${img}</td>
             <td>${item.name}</td>
             <td>${item.address_detail}</td>
             <td>${item.open_date}</td>
             <td>${item.close_date}</td>
+            <td>${item.status}</td>
             <td><a href="${urlEdit}"> Edit</a> | <a href="#" onclick="checkDelete(${item.id})">Delete</a></td>
         </tr>`;
         }
@@ -97,5 +132,6 @@
             deleteClinics(token, value)
         }
     }
+    });
 
 </script>
