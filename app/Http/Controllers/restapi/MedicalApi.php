d12497a79a995a4d2cd9c409bad4da7d97a6ae5a
dainqd <ngodaix5tp@gmail.com>
2023-12-09 12:30:11 +0700

    fix: admin doctor review.

--- app/Http/Controllers/restapi/MedicalApi.php
+++ app/Http/Controllers/restapi/MedicalApi.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\restapi;
 use App\Enums\TypeMedical;
 use App\Enums\UserStatus;
 use App\Http\Controllers\Controller;
-use App\Models\Department;
-use App\Models\Symptom;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -22,7 +20,7 @@ class MedicalApi extends Controller
         return response()->json($medicals);
     }
 
-    public function getMedical($type, $symptomID, $department)
+    private function getMedical($type, $symptomID, $department)
     {
         if (!$type) {
             $type = TypeMedical::DOCTORS;
@@ -38,5 +36,43 @@ class MedicalApi extends Controller
             ->orderBy('id', 'desc')
             ->get();
         return response()->json($listDoctor);
+    }
+
+    public function searchMedical(Request $request)
+    {
+        $star = $request->input('star');
+
+        $fullTime = $request->input('fulltime');
+        $experience = $request->input('experience');
+        $type = $request->input('type');
+        $medicals = $this->processSearchMedical($type, $star, $experience);
+        return response()->json($medicals);
+    }
+
+    private function processSearchMedical($type, $star, $experience)
+    {
+        if (!$type) {
+            $type = TypeMedical::DOCTORS;
+        }
+        $query = DB::table('users')
+            ->where('users.type', $type)
+            ->where('users.status', UserStatus::ACTIVE);
+
+        if ($experience) {
+            $aboutYear = explode('-', $experience);
+            $minYear = $aboutYear[0];
+            $maxYear = $aboutYear[1];
+            $query->whereBetween('users.year_of_experience', [$minYear, $maxYear]);
+        }
+
+        if ($star) {
+            $aboutStar = explode('-', $star);
+            $minStar = $aboutStar[0];
+            $maxStar = $aboutStar[1];
+            $query->whereBetween('users.average_star', [$minStar, $maxStar]);
+        }
+
+        $result = $query->orderBy('users.id', 'desc')->get();
+        return $result;
     }
 }
