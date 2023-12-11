f99d84b3f5e6c7fd0f29d378fc9e28550dff6ab6
kienh <kienhue98@gmail.com>
2023-12-11 17:40:06 +0700

    feat: modal forgot password

---
+++ app/Http/Controllers/SendSMSController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SendSMS;
+use Illuminate\Http\Request;
+
+class SendSMSController extends Controller
+{
+    //site, loginName, password, sendServiceCode, brandName, unicode, url
+    private $site;
+    private $loginName;
+    private $password;
+    private $sendServiceCode;
+    private $brandName;
+    private $unicode;
+
+    public function __construct()
+    {
+        $this->site = 'ILVIETNAM';
+        $this->loginName = 'admin';
+        $this->password = 'ILVIETNAM@1';
+        $this->sendServiceCode = '11';
+        $this->brandName = 'IL VIETNAM';
+        $this->unicode = '0';
+    }
+
+    private function sendSMS($user_id, $to, $content)
+    {
+        $SmsId = random_int(100000, 999999) . "-" . time();
+        $url = "https://api.s247.vn:8443/api/sms?site=" . $this->site . "&LoginName=" . $this->loginName
+            . "&Password=" . $this->password . "&SendServiceCode=" . $this->sendServiceCode . "&BrandName=" . $this->brandName
+            . "&mobile=" . $to . "&Message=" . $content . "&SmsId=" . $SmsId ."&Unicode=" . $this->unicode;
+
+        // call url by HTTP:get method
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', $url);
+        $response = json_decode($response->getBody(), true);
+        if ($response['code'] == 1) {
+            $this->WriteLogSMS($user_id, $to, $content);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function WriteLogSMS($user_id, $to, $content)
+    {
+        $sendSMS = new SendSMS();
+
+        $sendSMS->user_id_receiver = $user_id;
+        $sendSMS->contact_info_user_receiver = $to;
+        $sendSMS->content = $content;
+        $sendSMS->save();
+    }
+
+
+}
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,215 @@
-@php use App\Enums\Role;use App\Http\Middleware\MedicalPermission;use Illuminate\Support\Facades\Auth; @endphp
-@php @endphp
-@php @endphp
+@php use App\Enums\Role;use App\Enums\TypeTimeWork;use App\Http\Middleware\MedicalPermission;use Illuminate\Support\Facades\Auth; @endphp
+
+<style>
+
+    .h-80 {
+        height: 80%;
+    }
+    #heading {
+        text-transform: uppercase;
+        color: #673AB7;
+        font-weight: normal
+    }
+
+    #msform {
+        text-align: center;
+        position: relative;
+        margin-top: 20px
+    }
+
+    #msform fieldset {
+        background: white;
+        border: 0 none;
+        border-radius: 0.5rem;
+        box-sizing: border-box;
+        width: 100%;
+        margin: 0;
+        padding-bottom: 20px;
+        position: relative
+    }
+
+    .form-card {
+        text-align: left
+    }
+
+    #msform fieldset:not(:first-of-type) {
+        display: none
+    }
+
+    #msform input,
+    #msform textarea {
+        padding: 8px 15px 8px 15px;
+        border: 1px solid #ccc;
+        border-radius: 0px;
+        margin-bottom: 25px;
+        margin-top: 2px;
+        width: 100%;
+        box-sizing: border-box;
+        font-family: montserrat;
+        color: #2C3E50;
+        background-color: #ECEFF1;
+        font-size: 16px;
+        letter-spacing: 1px
+    }
+
+    #msform input:focus,
+    #msform textarea:focus {
+        -moz-box-shadow: none !important;
+        -webkit-box-shadow: none !important;
+        box-shadow: none !important;
+        border: 1px solid #673AB7;
+        outline-width: 0
+    }
+
+    #msform .action-button {
+        width: 100px;
+        background: #673AB7;
+        font-weight: bold;
+        color: white;
+        border: 0 none;
+        border-radius: 0px;
+        cursor: pointer;
+        padding: 10px 5px;
+        margin: 10px 0px 10px 5px;
+        float: right
+    }
+
+    #msform .action-button:hover,
+    #msform .action-button:focus {
+        background-color: #311B92
+    }
+
+    #msform .action-button-previous {
+        width: 100px;
+        background: #616161;
+        font-weight: bold;
+        color: white;
+        border: 0 none;
+        border-radius: 0px;
+        cursor: pointer;
+        padding: 10px 5px;
+        margin: 10px 5px 10px 0px;
+        float: right
+    }
+
+    #msform .action-button-previous:hover,
+    #msform .action-button-previous:focus {
+        background-color: #000000
+    }
+
+    .card {
+        z-index: 0;
+        border: none;
+        position: relative
+    }
+
+    .fs-title {
+        font-size: 25px;
+        color: #673AB7;
+        margin-bottom: 15px;
+        font-weight: normal;
+        text-align: left
+    }
+
+    .purple-text {
+        color: #673AB7;
+        font-weight: normal
+    }
+
+    .steps {
+        font-size: 25px;
+        color: gray;
+        margin-bottom: 10px;
+        font-weight: normal;
+        text-align: right
+    }
+
+    .fieldlabels {
+        color: gray;
+        text-align: left
+    }
+
+    #progressbar {
+        margin-bottom: 30px;
+        overflow: hidden;
+        color: lightgrey
+    }
+
+    #progressbar .active {
+        color: #673AB7
+    }
+
+    #progressbar li {
+        list-style-type: none;
+        font-size: 15px;
+        width: 33%;
+        float: left;
+        position: relative;
+        font-weight: 400
+    }
+
+    #progressbar #account:before {
+        font-family: FontAwesome;
+        content: "\f13e"
+    }
+
+    #progressbar #personal:before {
+        font-family: FontAwesome;
+        content: "\f007"
+    }
+
+    #progressbar #payment:before {
+        font-family: FontAwesome;
+        content: "\f030"
+    }
+
+    #progressbar #confirm:before {
+        font-family: FontAwesome;
+        content: "\f00c"
+    }
+
+    #progressbar li:before {
+        width: 50px;
+        height: 50px;
+        line-height: 45px;
+        display: block;
+        font-size: 20px;
+        color: #ffffff;
+        background: lightgray;
+        border-radius: 50%;
+        margin: 0 auto 10px auto;
+        padding: 2px
+    }
+
+    #progressbar li:after {
+        content: '';
+        width: 100%;
+        height: 2px;
+        background: lightgray;
+        position: absolute;
+        left: 0;
+        top: 25px;
+        z-index: -1
+    }
+
+    #progressbar li.active:before,
+    #progressbar li.active:after {
+        background: #673AB7
+    }
+
+    .progress {
+        height: 20px
+    }
+
+    .progress-bar {
+        background-color: #673AB7
+    }
+
+    .fit-image {
+        width: 100%;
+        object-fit: cover
+    }
+</style>
 <header class="container">
     <div class="main-header">
         <div class="before-header d-flex justify-content-between align-items-center">
@@ -281,7 +490,8 @@
                             <div class="form-element">
                                 <input id="remember-me" type="checkbox">
                                 <label for="remember-me">{{ __('home.Remember password') }}</label>
-                                <a href="#">{{ __('home.Forgot Password') }}?</a>
+                                <a href="#" data-toggle="modal" data-target="#modalForgetPassword">
+                                    {{ __('home.Forgot Password') }}</a>
                             </div>
                             <div class="form-element text-center">
                                 <button>{{ __('home.Login') }}</button>
@@ -317,7 +527,7 @@
 </div>
 <div class="modal fade" id="modalRegister" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
      aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content" id="modal_register">
             <div class="modal-body">
                 <div class="close-btn">
@@ -442,17 +652,17 @@
                                                 <label for="time_work">{{ __('home.Time work') }}</label>
                                                 <select class="form-select" id="time_work" name="time_work">
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::ALL}}">{{\App\Enums\TypeTimeWork::ALL}}</option>
+                                                        value="{{TypeTimeWork::ALL}}">{{TypeTimeWork::ALL}}</option>
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::NONE}}">{{\App\Enums\TypeTimeWork::NONE}}</option>
+                                                        value="{{TypeTimeWork::NONE}}">{{TypeTimeWork::NONE}}</option>
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::OFFICE_HOURS}}">{{\App\Enums\TypeTimeWork::OFFICE_HOURS}}</option>
+                                                        value="{{TypeTimeWork::OFFICE_HOURS}}">{{TypeTimeWork::OFFICE_HOURS}}</option>
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::ONLY_AFTERNOON}}">{{\App\Enums\TypeTimeWork::ONLY_MORNING}}</option>
+                                                        value="{{TypeTimeWork::ONLY_AFTERNOON}}">{{TypeTimeWork::ONLY_MORNING}}</option>
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::ONLY_AFTERNOON}}">{{\App\Enums\TypeTimeWork::ONLY_AFTERNOON}}</option>
+                                                        value="{{TypeTimeWork::ONLY_AFTERNOON}}">{{TypeTimeWork::ONLY_AFTERNOON}}</option>
                                                     <option
-                                                        value="{{\App\Enums\TypeTimeWork::OTHER}}">{{\App\Enums\TypeTimeWork::OTHER}}</option>
+                                                        value="{{TypeTimeWork::OTHER}}">{{TypeTimeWork::OTHER}}</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -519,6 +729,192 @@
         </div>
     </div>
 </div>
+
+<div class="modal modal-xl modal fade" id="modalForgetPassword" data-backdrop="static" data-keyboard="false"
+     tabindex="-1" aria-labelledby="modalForgetPasswordLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+        <div class="modal-content h-80" >
+
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-12 text-center p-0 mt-3 mb-2">
+                            <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
+                                <h2 id="heading">Lấy lại mật khẩu</h2>
+                                <form id="msform">
+                                    <!-- progressbar -->
+                                    <ul id="progressbar">
+                                        <li class="active" id="account"><strong>Account</strong></li>
+                                        <li id="personal"><strong>Personal</strong></li>
+                                        <li id="confirm"><strong>Finish</strong></li>
+                                    </ul>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped progress-bar-animated"
+                                             role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <br> <!-- fieldsets -->
+                                    <fieldset>
+                                        <div class="form-card">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2 class="fs-title">Account Information:</h2>
+                                                </div>
+                                                <div class="col-5">
+                                                    <h2 class="steps">Step 1 - 3</h2>
+                                                </div>
+                                            </div>
+                                            <label class="fieldlabels">Email: *</label> <input type="email" name="email"
+                                                                                               placeholder="Email Id"/>
+                                            <label class="fieldlabels">Username: *</label> <input type="text"
+                                                                                                  name="uname"
+                                                                                                  placeholder="UserName"/>
+                                        </div>
+                                        <input type="button" name="next" class="next action-button" value="Next"/>
+                                    </fieldset>
+                                    <fieldset>
+                                        <div class="form-card">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2 class="fs-title">Personal Information:</h2>
+                                                </div>
+                                                <div class="col-5">
+                                                    <h2 class="steps">Step 2 - 3</h2>
+                                                </div>
+                                            </div>
+                                            <label class="fieldlabels">First Name: *</label> <input type="text"
+                                                                                                    name="fname"
+                                                                                                    placeholder="First Name"/>
+                                            <label class="fieldlabels">Last Name: *</label> <input type="text"
+                                                                                                   name="lname"
+                                                                                                   placeholder="Last Name"/>
+                                            <label class="fieldlabels">Contact No.: *</label> <input type="text"
+                                                                                                     name="phno"
+                                                                                                     placeholder="Contact No."/>
+                                            <label class="fieldlabels">Alternate Contact No.: *</label> <input
+                                                type="text" name="phno_2" placeholder="Alternate Contact No."/>
+                                        </div>
+                                        <input type="button" name="next" class="next action-button" value="Next"/>
+                                        <input type="button" name="previous" class="previous action-button-previous"
+                                               value="Previous"/>
+                                    </fieldset>
+                                    <fieldset>
+                                        <div class="form-card">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2 class="fs-title">Finish:</h2>
+                                                </div>
+                                                <div class="col-5">
+                                                    <h2 class="steps">Step 3 - 3</h2>
+                                                </div>
+                                            </div>
+                                            <br><br>
+                                            <h2 class="purple-text text-center"><strong>SUCCESS !</strong></h2> <br>
+                                            <div class="row justify-content-center">
+                                                <div class="col-3"><img src="https://i.imgur.com/GwStPmg.png"
+                                                                        class="fit-image"></div>
+                                            </div>
+                                            <br><br>
+                                            <div class="row justify-content-center">
+                                                <div class="col-7 text-center">
+                                                    <h5 class="purple-text text-center">You Have Successfully Signed
+                                                        Up</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{--script modal forget password--}}
+<script>
+    $(document).ready(function () {
+
+        var current_fs, next_fs, previous_fs; //fieldsets
+        var opacity;
+        var current = 1;
+        var steps = $("fieldset").length;
+
+        setProgressBar(current);
+
+        $(".next").click(function () {
+
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+//Add Class Active
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+//show the next fieldset
+            next_fs.show();
+//hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+                step: function (now) {
+// for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    next_fs.css({'opacity': opacity});
+                },
+                duration: 500
+            });
+            setProgressBar(++current);
+        });
+
+        $(".previous").click(function () {
+
+            current_fs = $(this).parent();
+            previous_fs = $(this).parent().prev();
+
+//Remove class active
+            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+//show the previous fieldset
+            previous_fs.show();
+
+//hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+                step: function (now) {
+// for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    previous_fs.css({'opacity': opacity});
+                },
+                duration: 500
+            });
+            setProgressBar(--current);
+        });
+
+        function setProgressBar(curStep) {
+            var percent = parseFloat(100 / steps) * curStep;
+            percent = percent.toFixed();
+            $(".progress-bar")
+                .css("width", percent + "%")
+        }
+
+        $(".submit").click(function () {
+            return false;
+        })
+
+    });
+
+</script>
 <script>
     $(document).ready(function () {
         // Lắng nghe sự kiện onchange của các dropdown tỉnh, huyện, xã
@@ -636,6 +1032,7 @@
                 $('#services_info').attr('required', false);
             }
         }
+
         function loadHospital(value) {
             if (value == '{{Role::HOSPITALS}}') {
                 $('#element-hospital').show();
