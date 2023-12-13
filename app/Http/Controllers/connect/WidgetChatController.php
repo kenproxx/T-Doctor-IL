c5ed8d382b2d1874a2e64d6f7efe94529dfed9e8
kienh <kienhue98@gmail.com>
2023-12-13 15:00:11 +0700

    fix: tính số tin nhắn chưa đọc widget chat

--- app/Http/Controllers/connect/WidgetChatController.php
+++ app/Http/Controllers/connect/WidgetChatController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\connect;
 
+use App\Enums\MessageStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Chat;
+use App\Models\Message;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class WidgetChatController extends Controller
 {
@@ -23,10 +26,21 @@ class WidgetChatController extends Controller
 
         $userInfoArray = [];
 
+        $totalUnreadMessage = Chat::where([
+            ['to_user_id', '=', Auth::id()],
+            ['message_status', '=', MessageStatus::UNSEEN]
+        ])->count();
+
         // Vòng lặp qua mỗi user_id trong mảng filteredUserIds
         foreach ($uniqueUserIds as $userId) {
             // Gọi hàm getNameByID để lấy tên theo id
             $userName = User::getNameByID($userId);
+
+            $countUnreadMessage = Chat::where([
+                ['from_user_id', '=', $userId],
+                ['to_user_id', '=', Auth::id()],
+                ['message_status', '=', MessageStatus::UNSEEN]
+            ])->count();
 
             // get email
             $userEmail = User::getEmailByID($userId);
@@ -35,8 +49,18 @@ class WidgetChatController extends Controller
             $userAvt = User::getAvtByID($userId);
 
             // Thêm thông tin (id & name) vào mảng userInfoArray
-            $userInfoArray[] = ['id' => $userId, 'name' => $userName, 'email' => $userEmail, 'avt' => $userAvt];
+            $userInfoArray[] = [
+                'id' => $userId,
+                'name' => $userName,
+                'email' => $userEmail,
+                'avt' => $userAvt,
+                'count_unread_message' => $countUnreadMessage,
+                'total_unread_message' => $totalUnreadMessage
+            ];
         }
+
+        //lấy tổng các tin nhắn chưa đọc gửi tới user hiện tại
+
 
         return response()->json($userInfoArray);
     }
@@ -46,10 +70,25 @@ class WidgetChatController extends Controller
         // lấy danh sách user đã kết nối với user hiện tại qua bảng chat
         $currentUserId = auth()->user()->id;
         $listMessageByUser = Chat::where(function ($query) use ($currentUserId, $id) {
-            $query->where([['from_user_id', $currentUserId], ['to_user_id', $id]])
-            ->orWhere([['from_user_id', $id], ['to_user_id', $currentUserId]]);
+            $query->where([['from_user_id', $currentUserId], ['to_user_id', $id]])->orWhere([
+                ['from_user_id', $id],
+                ['to_user_id', $currentUserId]
+            ]);
         })->get();
 
         return response()->json($listMessageByUser);
+    }
+
+    public function handleSeenMessage($id)
+    {
+        if (!$id) {
+            return;
+        }
+
+        Message::where([['from', '=', $id], ['to', '=', Auth::id()]])->update(['read' => true]);
+        Chat::where([
+            ['from_user_id', '=', $id],
+            ['to_user_id', '=', Auth::id()]
+        ])->update(['message_status' => MessageStatus::SEEN]);
     }
 }
--- resources/views/components/head/chat-message.blade.php
+++ resources/views/components/head/chat-message.blade.php
@@ -388,6 +388,8 @@
 
     let currentId = '{{ Auth::check() ? Auth::user()->id : '' }}';
 
+    let totalMessageUnseen = 0;
+
     window.Pusher = Pusher;
     window.Echo = new Echo({
         broadcaster: 'pusher',
@@ -398,18 +400,82 @@
 
     window.Echo.private("messages." + currentId).listen('NewMessage', function (e) {
         renderMessageReceive(e);
+        handleSeenMessage();
+        calculateTotalMessageUnseen(e);
     });
+
+    function handleSeenMessage() {
+        if (!chatUserId) {
+            return;
+        }
+
+        let url = `{{ route('api.backend.connect.chat.seen-message', ['id' => ':id']) }}`;
+        url = url.replace(':id', chatUserId);
+
+        $.ajax({
+            url: url,
+            type: "GET",
+            dataType: "json",
+
+            success: function (data) {
+                console.log(data)
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+
+    }
+
+    function calculateTotalMessageUnseen(e) {
+        console.log(e)
+        console.log(chatUserId)
+
+        if (e.message.from === chatUserId) {
+            console.log(123)
+            return;
+        }
+        console.log(456)
+
+        totalMessageUnseen++;
+
+        console.log('totalMessageUnseen', totalMessageUnseen)
+
+        $('#totalMsgUnseen').html(totalMessageUnseen);
+
+        // duyệt class friend, tìm ra div có data-id = e.message.from
+        // tăng thêm 1 span.badge
+        $(".friend").each(function () {
+
+            if ($(this).data('id') === e.message.from) {
+                let countUnseen = $(this).data('msg-unseen');
+                countUnseen++;
+                $(this).data('msg-unseen', countUnseen);
+
+                $(this).find('span.badge-light').html(countUnseen);
+            }
+
+        });
+    }
 
     function genListUserWasConnect(data) {
         let html = '';
         $.each(data, function (index, item) {
-            html += `<div class="friend" data-id=${item.id}>
+            let countUnseen = item.count_unread_message;
+            if (countUnseen === 0) {
+                countUnseen = '';
+            }
+
+            html += `<div class="friend" data-id=${item.id} data-msg-unseen="${item.count_unread_message}">
                         <img src="${item.avt}"/>
                         <p>
-                            <strong>${item.name}</strong>
+                            <strong>${item.name}
+                                <span class="badge badge-light text-black">${countUnseen}</span>
+                            </strong>
+                                <br>
                             <span>${item.email}</span>
                         </p>
-                    </div>`
+                    </div>`;
         });
 
         $('#friends').html(html);
@@ -433,7 +499,9 @@
         $(".friend").each(function () {
             $(this).click(function () {
                 chatUserId = $(this).data('id');
+                handleSeenMessage();
                 handleStartChat(chatUserId);
+                removeSpanBadges(this);
 
                 var childOffset = $(this).offset();
                 var parentOffset = $(this).parent().parent().offset();
@@ -461,6 +529,7 @@
 
 
                 $('#close').unbind("click").click(function () {
+                    chatUserId = '';
                     $("#chat-messages, #profile, #profile p").removeClass("animate");
 
                     setTimeout(function () {
@@ -471,6 +540,20 @@
                 autoScrollChatBox();
             });
         });
+    }
+
+    function removeSpanBadges(divElement) {
+        $(divElement).find('span.badge').html('');
+
+        let countUnseen = $(divElement).data('msg-unseen');
+
+        totalMessageUnseen -= countUnseen;
+
+        if (totalMessageUnseen <= 0) {
+            $('#totalMsgUnseen').html('');
+        } else {
+            $('#totalMsgUnseen').html(totalMessageUnseen);
+        }
     }
 
     // Gắn sự kiện keyup cho input
@@ -542,6 +625,7 @@
         });
 
         $(".chat-box-toggle").click(function () {
+            chatUserId = '';
             $("#chat-circle").toggle("scale");
             $(".chat-box").toggle("scale");
         });
@@ -621,11 +705,23 @@
             dataType: "json",
             success: function (data) {
                 genListUserWasConnect(data);
+                renderTotalMessageUnseen(data);
             },
             error: function (e) {
                 console.log(e);
             }
         });
+    }
+
+    function renderTotalMessageUnseen(data) {
+        if (data.length == 0) {
+            return;
+        }
+        totalMessageUnseen = data[0]['total_unread_message'];
+
+        if (totalMessageUnseen > 0) {
+            $('#chat-circle').append(`<span class="badge badge-light text-black" id="totalMsgUnseen">${totalMessageUnseen}</span>`);
+        }
     }
 
     getListUserWasConnect();
