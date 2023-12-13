9871d59ae402f49d0e94289f02918a5287813652
Quy Nguyen <quy.nguyen@192.168.2.7>
2023-12-13 10:13:25 +0700

    create api get get list department

--- database/migrations/2023_12_11_081438_add_prescription_to_users_table.php
+++ database/migrations/2023_12_11_081438_add_prescription_to_users_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chat_messages', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('prescription')->nullable();
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chat_messages');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
