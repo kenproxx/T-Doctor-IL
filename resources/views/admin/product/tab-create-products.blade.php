bf9df2332535e4627e599b31f0c66181f5abbcd1
dainqd <ngodaix5tp@gmail.com>
2023-12-20 16:25:43 +0700

    login only device.

--- resources/views/admin/product/tab-create-products.blade.php
+++ resources/views/admin/product/tab-create-products.blade.php
@@ -14,20 +14,20 @@
             </button>
         </div>
     @endif
-    <form id="form">
+    <form id="form-create-product" action="#" method="post">
         <div>
             <div class="row">
                 <div class="col-md-4">
                     <label for="name">{{ __('home.Name') }}</label>
-                    <input type="text" class="form-control" id="name" name="name" value="">
+                    <input type="text" class="form-control" id="name" name="name" required>
                 </div>
                 <div class="col-md-4">
                     <label for="name_en">{{ __('home.name_en') }}</label>
-                    <input type="text" class="form-control" id="name_en" name="name_en" value="">
+                    <input type="text" class="form-control" id="name_en" name="name_en" required>
                 </div>
                 <div class="col-md-4">
                     <label for="name_laos">{{ __('home.name_laos') }}</label>
-                    <input type="text" class="form-control" id="name_laos" name="name_laos" value="">
+                    <input type="text" class="form-control" id="name_laos" name="name_laos" required>
                 </div>
             </div>
 
@@ -46,17 +46,17 @@
                 <div class="col-md-4">
                     <label for="brand_name">{{ __('home.Brand Name') }}</label>
                     <input type="text" class="form-control" id="brand_name" name="brand_name"
-                           value="">
+                           value="" required>
                 </div>
                 <div class="col-md-4">
                     <label for="brand_name_en">{{ __('home.Brand Name English') }}</label>
                     <input type="text" class="form-control" id="brand_name_en" name="brand_name_en"
-                           value="">
+                           value="" required>
                 </div>
                 <div class="col-md-4">
                     <label for="brand_name_laos">{{ __('home.Brand Name Laos') }}</label>
                     <input type="text" class="form-control" id="brand_name_laos" name="brand_name_laos"
-                           value="">
+                           value="" required>
                 </div>
             </div>
             <div class="row">
@@ -82,7 +82,7 @@
             </div>
             <div>
                 <label>{{ __('home.Thumbnail') }}</label>
-                <input type="file" class="form-control" id="thumbnail" name="thumbnail" multiple accept="image/*">
+                <input type="file" class="form-control" id="thumbnail" name="thumbnail" multiple accept="image/*" required>
             </div>
             <div>
                 <label>{{ __('home.gallery') }}</label>
@@ -91,16 +91,16 @@
             <div class="row">
                 <div class="col-md-6">
                     <label for="price">{{ __('home.Price') }}</label>
-                    <input type="number" class="form-control" id="price" name="price" value="">
+                    <input type="number" required class="form-control" id="price" name="price" value="">
                 </div>
                 <div class="col-md-6">
                     <label for="price_unit">{{ __('home.Price Unit') }}</label>
                     <input type="text" class="form-control" id="price_unit" name="price_unit"
-                           value="VND">
+                           value="VND" required>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <label for="ads_plan">{{ __('home.Ads Plan') }}</label>
                     <select id="ads_plan" name="ads_plan" class="form-select">
                         <option value="1">{{ __('home.Platinum') }}</option>
@@ -108,7 +108,7 @@
                         <option value="3">{{ __('home.Silver') }}</option>
                     </select>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <label for="ads_period">{{ __('home.Ads Period') }}</label>
                     <select id="ads_period" name="ads_period" class="form-select">
                         <option value="1">{{ __('home.5 Day') }}</option>
@@ -117,76 +117,83 @@
                         <option value="4">{{ __('home.20 Day') }}</option>
                     </select>
                 </div>
-            </div>
-            <div hidden="">
-                <label for="status">{{ __('home.Status') }}</label>
-                <input type="text" class="form-control" id="status" name="status" value="">
+                <div class="col-md-4">
+                    <label for="status">{{ __('home.Status') }}</label>
+                    <select id="status" name="status" class="form-select">
+                        <option value="{{ \App\Enums\ProductStatus::ACTIVE }}">{{ \App\Enums\ProductStatus::ACTIVE }}</option>
+                        <option value="{{ \App\Enums\ProductStatus::ACTIVE }}">{{ \App\Enums\ProductStatus::ACTIVE }}</option>
+                    </select>
+                </div>
             </div>
             <div hidden="">
                 <label for="user_id">{{ __('home.Username') }}</label>
                 <input type="text" class="form-control" id="user_id" name="user_id" value="{{Auth::user()->id}}">
             </div>
         </div>
-        <button type="button" class="btn btn-primary up-date-button mt-md-4">{{ __('home.Save') }}</button>
+        <button type="submit" class="btn btn-primary up-date-button mt-md-4">{{ __('home.Save') }}</button>
     </form>
     <script>
         $(document).ready(function () {
-            $('.up-date-button').on('click', function () {
-                const headers = {
-                    'Authorization': `Bearer ${token}`
-                };
-                const formData = new FormData();
-                formData.append("name", $('#name').val());
-                formData.append("name_en", $('#name_en').val());
-                formData.append("name_laos", $('#name_laos').val());
-                formData.append("category_id", $('#category_id').val());
-                formData.append("brand_name", $('#brand_name').val());
-                formData.append("brand_name_en", $('#brand_name_en').val());
-                formData.append("brand_name_laos", $('#brand_name_laos').val());
-                formData.append("province_id", $('#province_id').val());
-                formData.append("price", $('#price').val());
-                formData.append("price_unit", $('#price_unit').val());
-                formData.append("ads_plan", $('#ads_plan').val());
-                formData.append("ads_period", $('#ads_period').val());
-                formData.append("user_id", $('#user_id').val());
-                const fieldTextareaTiny = ["description", "description_en", "description_laos"
-                ];
-                fieldTextareaTiny.forEach(fieldTextarea => {
-                    const content = tinymce.get(fieldTextarea).getContent();
-                    formData.append(fieldTextarea, content);
-                });
-
-                var filedata = document.getElementById("gallery");
-                var i = 0, len = filedata.files.length, img, reader, file;
-                for (i; i < len; i++) {
-                    file = filedata.files[i];
-                    formData.append('gallery[]', file);
-                }
-                const photo = $('#thumbnail')[0].files[0];
-                formData.append('thumbnail', photo);
-                formData.append('status', 'ACTIVE');
-                console.log(formData)
-                try {
-                    $.ajax({
-                        url: `{{route('api.backend.products.create')}}`,
-                        method: 'POST',
-                        headers: headers,
-                        contentType: false,
-                        cache: false,
-                        processData: false,
-                        data: formData,
-                        success: function (response) {
-                            alert('success');
-                            window.location.href = `{{route('homeAdmin.list.product')}}`;
-                        },
-                        error: function (exception) {
-                            console.log(exception)
-                        }
-                    });
-                } catch (error) {
-                    throw error;
-                }
-            })
+            $('#form-create-product').submit(function (e) {
+                e.preventDefault();
+                createProduct();
+            });
         })
+
+        async function createProduct() {
+            const headers = {
+                'Authorization': `Bearer ${token}`
+            };
+            const formData = new FormData();
+            formData.append("name", $('#name').val());
+            formData.append("name_en", $('#name_en').val());
+            formData.append("name_laos", $('#name_laos').val());
+            formData.append("category_id", $('#category_id').val());
+            formData.append("brand_name", $('#brand_name').val());
+            formData.append("brand_name_en", $('#brand_name_en').val());
+            formData.append("brand_name_laos", $('#brand_name_laos').val());
+            formData.append("province_id", $('#province_id').val());
+            formData.append("price", $('#price').val());
+            formData.append("price_unit", $('#price_unit').val());
+            formData.append("ads_plan", $('#ads_plan').val());
+            formData.append("ads_period", $('#ads_period').val());
+            formData.append("user_id", $('#user_id').val());
+            const fieldTextareaTiny = ["description", "description_en", "description_laos"
+            ];
+            fieldTextareaTiny.forEach(fieldTextarea => {
+                const content = tinymce.get(fieldTextarea).getContent();
+                formData.append(fieldTextarea, content);
+            });
+
+            var filedata = document.getElementById("gallery");
+            var i = 0, len = filedata.files.length, img, reader, file;
+            for (i; i < len; i++) {
+                file = filedata.files[i];
+                formData.append('gallery[]', file);
+            }
+            const photo = $('#thumbnail')[0].files[0];
+            formData.append('thumbnail', photo);
+            formData.append('status', 'ACTIVE');
+            try {
+                await $.ajax({
+                    url: `{{route('api.backend.products.create')}}`,
+                    method: 'POST',
+                    headers: headers,
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                    data: formData,
+                    success: function (response) {
+                        alert('success');
+                        window.location.href = `{{route('homeAdmin.list.product')}}`;
+                    },
+                    error: function (exception) {
+                        console.log(exception)
+                    }
+                });
+            } catch (error) {
+                throw error;
+            }
+        }
     </script>
 @endsection
