4be51d0d899d9d847f897eb8461cdbf308571007
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2023-12-21 14:56:28 +0700

    fix: filter online medicine

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -58,50 +58,80 @@ class MedicineController extends Controller
 
     public function searchOnlineMedicine(Request $request)
     {
-        // Bắt đầu với tất cả các sản phẩm y tế được phê duyệt
         $medicines = ProductMedicine::where('product_medicines.status', OnlineMedicineStatus::APPROVED);
 
-        // Lấy giá trị từ các tham số yêu cầu
+        $name = $request->input('name');
         $filter = $request->input('filter');
         $object = $request->input('object');
         $price = $request->input('price');
+        $min_price = $request->input('min_price');
+        $max_price = $request->input('max_price');
         $category = $request->input('category');
         $location = $request->input('location');
-        // Kiểm tra và áp dụng điều kiện tìm kiếm
-        if ($filter) {
-            $filterValues = explode(',', $filter);
-            //kiểm tra nếu trong $filterValues có phần tử 0, => filter = ''
 
+
+
+        // Tìm kiếm theo filter
+        $medicines->when($filter, function ($query) use ($filter) {
+            $filterValues = explode(',', $filter);
             $ifFilterValuesHasZero = in_array(0, $filterValues);
             if (!$ifFilterValuesHasZero) {
-                $medicines->whereIn('filter_', $filterValues);
+                $query->whereIn('filter_', $filterValues);
             }
-        }
-        if ($object) {
-            $objectValues = explode(',', $object);
-            $medicines->whereIn('object_', $objectValues);
-        }
-        if ($price) {
-            $medicines->where('price', '<=', $price);
-        }
-        if ($category) {
-            $medicines->where('category_id', $category);
-        }
-        if ($location) {
-            $medicines->where('provinces.id', $location);
-        }
+        });
 
-        $medicines
-            ->leftJoin('users', 'product_medicines.user_id', '=', 'users.id')
+        // Tìm kiếm theo object
+        $medicines->when($object, function ($query) use ($object) {
+            $objectValues = explode(',', $object);
+            $query->whereIn('object_', $objectValues);
+        });
+
+        // Tìm kiếm theo giá
+        $medicines->when($min_price, function ($query) use ($min_price) {
+            $query->where('price', '>=', $min_price);
+        });
+
+        $medicines->when($max_price, function ($query) use ($max_price) {
+            $query->where('price', '<=', $max_price);
+        });
+
+        // Tìm kiếm theo category
+        $medicines->when($category, function ($query) use ($category) {
+            $query->where('category_id', $category);
+        });
+
+        // Tìm kiếm theo location
+        $medicines->when($location, function ($query) use ($location) {
+            $query->where('provinces.id', $location);
+        });
+        // Tìm kiếm theo tên
+        $medicines->when($name, function ($query) use ($name) {
+            $query->where(function ($query) use ($name) {
+                $query->orWhere('product_medicines.name', 'like', "%$name%")
+                    ->orWhere('product_medicines.name_en', 'like', "%$name%")
+                    ->orWhere('product_medicines.name_laos', 'like', "%$name%");
+            });
+        });
+
+
+        // Join và select
+        $medicines->leftJoin('users', 'product_medicines.user_id', '=', 'users.id')
             ->leftJoin('provinces', 'provinces.id', '=', 'users.province_id')
             ->select('product_medicines.*', 'provinces.name as location_name');
 
-        // Lấy dữ liệu đã lọc và phân trang
-        $medicines = $medicines->paginate();
+        // Paginate
+        $medicines = $medicines->paginate(15);
 
-        // Hiển thị dữ liệu đã lọc
+        // Trả về kết quả
+        $response = [
+            'min_price' => $min_price,
+            'max_price' => $max_price,
+            'data' => $medicines,
+        ];
+
         return response()->json($medicines);
     }
+
 
     public function getLocationByUserId(Request $request)
     {
