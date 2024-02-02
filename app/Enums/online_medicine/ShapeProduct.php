801e45a9fb1a4dec54ad00ad47d8c779a20aaace
dainqd <ngodaixtp@gmail.com>
2024-02-02 17:07:09 +0700

    Upgrade column in ProductMedicine.

---
+++ app/Enums/online_medicine/ShapeProduct.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums\online_medicine;
+
+use BenSampo\Enum\Enum;
+
+final class ShapeProduct extends Enum
+{
+    /* Dạng thuốc tiêm */
+    const INJECTION = 'INJECTION';
+    /* Dạng dung dịch */
+    const SOLUTION = 'SOLUTION';
+    /* Dạng viên sủi */
+    const EFFERVESCENT = 'EFFERVESCENT';
+    /* Dạng bột */
+    const POWDER = 'POWDER';
+    /* Dạng viên nén */
+    const TABLET = 'TABLET';
+    /* Dạng viên nang */
+    const CAPSULE = 'CAPSULE';
+    /* Dạng khác */
+    const OTHER = 'Other';
+}
