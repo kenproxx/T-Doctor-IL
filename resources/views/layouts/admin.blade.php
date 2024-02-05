f952cd077b7dd3f3fe42c12d1a326aa3271f2514
kienh <kienhue98@gmail.com>
2024-02-05 11:27:38 +0700

    chore: thay doi favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,8 @@
     <title> @yield('title')</title>
     <meta content="krmedi description" name="description">
     <meta content="krmedi" name="keywords">
+    <link rel="icon" type="image/png"
+          href="{{ asset('img/logo.png') }}">
     <!-- Favicons -->
     <link href="{{ asset('admin/img/favicon.png')}}" rel="icon">
     <link href="{{ asset('admin/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
