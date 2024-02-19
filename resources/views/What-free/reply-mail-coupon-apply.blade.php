e85f475817d2738b0d0de9305e5e4d9f7f32e646
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2024-02-19 15:37:18 +0700

    feat: reply link social

---
+++ resources/views/What-free/reply-mail-coupon-apply.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.master')
+@section('title', 'What free')
+@section('content')
+
+    <link href="{{ asset('css/reply-link.css') }}" rel="stylesheet">
+    @include('layouts.partials.header')
+    @include('component.banner')
+    @auth
+        @php
+            $couponApply = \App\Models\CouponApply::where('id', $id)->where('user_id', Auth::user()->id)->first();
+        @endphp
+        <div class="container">
+            @if($couponApply != null)
+                <form action="{{route('what.free.reply.link.social',$couponApply->id)}}" method="post">
+                    @csrf
+                    @method('POST')
+                    <div class="container">
+                        <h1>Form nhập thông tin review</h1>
+                        <p>Xin hãy nhập biểu mẫu bên dưới.</p>
+                        <hr>
+                        @php
+                            $sns_option =$couponApply->sns_option;
+                            $sns_option = explode(',',$sns_option);
+                        @endphp
+                        <div>
+                            <label for="link_fb"><b>thông tin review FaceBook</b></label>
+                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_fb" id="link_fb">
+                        </div>
+                        <div>
+                            <label for="link_tt"><b>thông tin review Tiktok</b></label>
+                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_tt" id="link_tt">
+                        </div>
+                        <div>
+                            <label for="link_ig"><b>thông tin review Instagram</b></label>
+                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_ig" id="link_ig">
+                        </div>
+                        <div>
+                            <label for="link_yt"><b>thông tin review Youtube</b></label>
+                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_yt" id="link_yt">
+                        </div>
+                        <div>
+                            <label for="link_gg"><b>thông tin review Google Review</b></label>
+                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_gg" id="link_gg">
+                        </div>
+                        <div class="clearfix">
+                            <button type="submit" class="signupbtn">Sign Up</button>
+                        </div>
+                    </div>
+                </form>
+            @else
+                <div class="container d-flex justify-content-center">
+                    <h4>Tài khoản này chưa được ứng tuyển</h4>
+
+                </div>
+            @endif
+
+            @else
+                <div class="container d-flex justify-content-center">
+                    <h4>BAN CHƯA ĐĂNG NHẬP</h4>
+
+                </div>
+            @endauth
+        </div>
+
+
+
+        @endsection
