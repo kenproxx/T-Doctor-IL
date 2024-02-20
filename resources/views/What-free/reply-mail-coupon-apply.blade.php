db1389bf3897a85e78a8018d120998dfd86ec23f
Hải-Thành <105226959+Hai-Thanh@users.noreply.github.com>
2024-02-20 10:43:34 +0700

    fix: reply link social commit

--- resources/views/What-free/reply-mail-coupon-apply.blade.php
+++ resources/views/What-free/reply-mail-coupon-apply.blade.php
@@ -5,53 +5,44 @@
     <link href="{{ asset('css/reply-link.css') }}" rel="stylesheet">
     @include('layouts.partials.header')
     @include('component.banner')
-        @php
-            $couponApply = \App\Models\CouponApply::where('id', $id)->first();
-        @endphp
-        <div class="container">
-            @if($couponApply->content == null)
-                <form action="{{route('what.free.reply.link.social',$couponApply->id)}}" method="post">
-                    @csrf
-                    @method('POST')
-                    <div class="container">
-                        <h1>Form nhập thông tin review</h1>
-                        <p>Xin hãy nhập biểu mẫu bên dưới.</p>
-                        <hr>
-                        @php
-                            $sns_option =$couponApply->sns_option;
-                            $sns_option = explode(',',$sns_option);
-                        @endphp
-                        <div>
-                            <label for="link_fb"><b>Thông tin review FaceBook</b></label>
-                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_fb" id="link_fb">
-                        </div>
-                        <div>
-                            <label for="link_tt"><b>Thông tin review Tiktok</b></label>
-                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_tt" id="link_tt">
-                        </div>
-                        <div>
-                            <label for="link_ig"><b>Thông tin review Instagram</b></label>
-                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_ig" id="link_ig">
-                        </div>
-                        <div>
-                            <label for="link_yt"><b>Thông tin review Youtube</b></label>
-                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_yt" id="link_yt">
-                        </div>
-                        <div>
-                            <label for="link_gg"><b>Thông tin review Google Review</b></label>
-                            <input class="link-social" type="text" placeholder="Nhập Link" name="link_gg" id="link_gg">
-                        </div>
-                        <div class="clearfix">
-                            <button type="submit" class="signupbtn">Sign Up</button>
-                        </div>
-                    </div>
-                </form>
-            @else
-                <div class="container d-flex justify-content-center">
-                    <h4>Bạn đã gửi liên kết đăng bài</h4>
+    @php
+        $couponApply = \App\Models\CouponApply::where('id', $id)->first();
+    @endphp
+    <div class="container">
+        @if($couponApply->content == null)
+            <form action="{{route('what.free.reply.link.social',$couponApply->id)}}" method="post">
+                @csrf
+                @method('POST')
+                <div class="container">
+                    <h1>{{ __('home.Form nhập thông tin review') }}</h1>
+                    <p>{{ __('home.Xin hãy nhập biểu mẫu bên dưới.') }}</p>
+                    <hr>
+                    @php
+                        $sns_option =$couponApply->sns_option;
+                        $sns_option = explode(', ',$sns_option);
+                    @endphp
 
+                    @foreach($sns_option as $sns_social)
+                        @foreach(['tiktok', 'facebook', 'instagram', 'youtube', 'google_review'] as $platform)
+                            @if($sns_social == $platform)
+                                <div>
+                                    <label for="{{$sns_social}}"><b>{{ __('home.Thông tin review ') }} {{$sns_social}}</b></label>
+                                    <input class="link-social" type="text" placeholder="Nhập Link" name="{{$sns_social}}"
+                                           id="{{$sns_social}}" required>
+                                </div>
+                            @endif
+                        @endforeach
+                    @endforeach
+                    <div class="clearfix">
+                        <button type="submit" class="signupbtn">{{ __('home.Submit') }}</button>
+                    </div>
                 </div>
-            @endif
+            </form>
+        @else
+            <div class="container d-flex justify-content-center">
+                <h4>Bạn đã gửi liên kết đăng bài</h4>
+            </div>
+        @endif
 
         </div>
 
