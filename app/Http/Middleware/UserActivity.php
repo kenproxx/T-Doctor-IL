723afba6046cadac3b82dfa2a67132602eb8c54c
kienh <kienhue98@gmail.com>
2024-02-20 17:26:55 +0700

    fix: get user online from app

--- app/Http/Middleware/UserActivity.php
+++ app/Http/Middleware/UserActivity.php
@@ -4,27 +4,39 @@ namespace App\Http\Middleware;
 
 use App\Models\User;
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserActivity
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check()) {
+        try {
+
+            if (!Auth::check()) {
+                $user = JWTAuth::parseToken()->authenticate();
+            } else {
+                $user = Auth::user();
+            }
+
             $expiresAt = now()->addMinutes(2); /* keep online for 2 min */
-            Cache::put('user-is-online|' . Auth::user()->id, true, $expiresAt);
+            Cache::put('user-is-online|' . $user->id, true, $expiresAt);
 
             /* last seen */
-            User::where('id', Auth::user()->id)->update(['last_seen' => now()]);
+            User::where('id', $user->id)->update(['last_seen' => now()]);
+        } catch (Exception $e) {
         }
+
         return $next($request);
+
     }
 }
